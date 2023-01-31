0d5a7deb690835faef36e943f6afee206e09d3ff
income3000 <income3000@gmail.com>
2023-01-31 05:11:29 -0500

    initial commit

---
+++ odin1/reactf/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+      
+      <div>
+        this is for the homies
+      </div>
+      <input type="date" />
+      <input type="text" placeholder='write here'/>
+      <br />
+      
+      <footer>where am i</footer>
+    </div>
+  );
+}
+
+export default App;
